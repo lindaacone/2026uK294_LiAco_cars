ee6cb53917e4bf5c35e5a80cda14a0bc86b28fed
lindaacone <linda.acone@edu.tbz.ch>
2026-05-22 16:11:18 +0200

    Add protected routes and deleted texts

--- 2026uK294_LiAco_cars/src/App.tsx
+++ 2026uK294_LiAco_cars/src/App.tsx
@@ -1,21 +1,52 @@
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LoginPage from "./components/pages/LoginPage";
 import ObjectListPage from "./components/pages/ObjectListPage";
 import ObjectDetailPage from "./components/pages/ObjectDetailPage";
 import ObjectEditPage from "./components/pages/ObjectEditPage";
-import Test from "./Test";
+import ProtectedRoute from "./routes/ProtectedRoute";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/test" element={<Test />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/objects" element={<ObjectListPage />} />
-        <Route path="/objects/:id" element={<ObjectDetailPage />} />
-        <Route path="/objects/:id/edit" element={<ObjectEditPage />} />
-        <Route path="/objects/create" element={<ObjectEditPage />} />
-        <Route path="*" element={<Navigate to="/login" />} />
+
+        
+        <Route path="/login" element={ 
+      <ProtectedRoute>
+        <LoginPage />
+    </ProtectedRoute>
+  }
+/>
+
+        <Route path="/objects" element={ 
+      <ProtectedRoute>
+        <ObjectListPage />
+    </ProtectedRoute>
+  }
+/>
+
+<Route path="/objects/:id" element={
+    <ProtectedRoute>
+      <ObjectDetailPage />
+    </ProtectedRoute>
+  }
+/>
+
+<Route
+  path="/objects/:id/edit" element={
+    <ProtectedRoute>
+      <ObjectEditPage />
+    </ProtectedRoute>
+  }
+/>
+
+<Route
+  path="/objects/create" element={
+    <ProtectedRoute>
+      <ObjectEditPage />
+    </ProtectedRoute>
+  }
+/>
       </Routes>
     </BrowserRouter>
   )
---
+++ 2026uK294_LiAco_cars/src/routes/ProtectedRoute.tsx
@@ -0,0 +1,17 @@
+import { Navigate } from "react-router-dom";
+
+interface ProtectedRouteProps {
+  children: React.ReactNode;
+}
+
+export default function ProtectedRoute({
+  children,
+}: ProtectedRouteProps) {
+  const token = localStorage.getItem("accessToken");
+
+  if (!token) {
+    return <Navigate to="/login" />;
+  }
+
+  return <>{children}</>;
+}
